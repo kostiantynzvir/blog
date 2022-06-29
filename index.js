3cfbb5d1f9b90c6069881a04ff56e63fb4471511
prodev205 <godmiracle205@gmail.com>
2022-06-29 17:24:47 -0300

    Mobile menu added

---
+++ index.js
@@ -0,0 +1,11 @@
+function openMenu() {
+  const openButton = document.getElementById('header')
+
+  openButton.classList.add('opened-menu')
+}
+
+function closeMenu() {
+  const openButton = document.getElementById('header')
+
+  openButton.classList.remove('opened-menu')
+}
